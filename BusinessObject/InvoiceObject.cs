fcabbcfbf37bbcf317d150401c5cd7d8a70f1642
ThienNH <thiennhse184989@fpt.edu.vn>
2024-07-26 02:20:22 +0700

    Add Invoice wpf

---
+++ BusinessObject/InvoiceObject.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class InvoiceObject
+    {
+        private readonly IPaymentRepository paymentRepository;
+
+        public InvoiceObject()
+        {
+            this.paymentRepository = new PaymentRepository();
+        }
+        public InvoiceViewModel CreateInvoice(int bookingId) => paymentRepository.CreateInvoiceView(bookingId);
+    }
+}
--- DataAccess/Repository/IPaymentRepository.cs
+++ DataAccess/Repository/IPaymentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace DataAccess.Repository
 {
     public interface IPaymentRepository
     {
-
+        public Payment? GetPaymentById(int id);
+        public List<Payment> GetAllPayment();
+        public void InsertPayment(Payment payment);
+        public void UpdatePayment(Payment payment);
+        public void DeletePayment(int id);
+        public InvoiceViewModel CreateInvoiceView(int bookingId);
     }
 }
